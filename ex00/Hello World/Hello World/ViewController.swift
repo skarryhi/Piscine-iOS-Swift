871f0ac9269ed3da8a98f9305e34730e62cc8d76
skarry <60601122@mail.ru>
2021-09-20 11:57:53 +0300

    ex00

---
+++ ex00/Hello World/Hello World/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  Hello World
+//
+//  Created by Анна Заблуда on 20.09.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+    }
+
+    @IBAction func clickedButton(_ sender: Any) {
+        print("Hello World!")
+    }
+    
+}
+
